723c998c4e940a8bd6892ec85f4d4bd57fc2e813
Vishav Bangotra <vishavbangotra@Vishavs-MacBook-Air.local>
2020-08-26 16:20:51 +0530

    Created database and APIs

--- spotifizz/core/models.py
+++ spotifizz/core/models.py
@@ -1,5 +1,38 @@
 from django.db import models
+import os
+from spotifizz import settings
+class Seller(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='seller/', height_field=None, width_field=None, max_length=None)
 
-class postgressetup(models.Model):
-    name = models.CharField(max_length = 256)
-    roll = models.IntegerField()
+    def __str__(self):
+        return self.user.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    image = models.ImageField(upload_to='product/', height_field=None, width_field=None, max_length=None)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    date_published = models.DateField(auto_now_add=True)
+    desciption = models.TextField(max_length=1028, null=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.product
+
+class Order(models.Model):
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(ProductOrder)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+
--- spotifizz/spotifizz/settings.py
+++ spotifizz/spotifizz/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'spotifizz',
-        'USER': 'vishavbangotra',
+        'USER': 'postgres',
         'PASSWORD': '0803',
         'HOST': 'localhost',
     }
