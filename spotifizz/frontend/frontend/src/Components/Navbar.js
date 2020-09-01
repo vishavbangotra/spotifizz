a15c3e6b97f1c78b8dbd5a23df8c2e2db11861c1
Vishav Bangotra <vishavbangotra@Vishavs-MacBook-Air.local>
2020-09-01 16:45:31 +0530

    I am starting to work in react end

---
+++ spotifizz/frontend/frontend/src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Navbar extends Component{
+  render(){
+    return(
+      <div>Hello WOrld</div>
+    )
+  }
+}
+export default Navbar;
